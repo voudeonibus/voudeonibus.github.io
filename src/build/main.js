3d45ebb36f072324a52c8b83299305a9ba9e76cc
Danilo Woznica <danilowoznica@MacBook-Pro-de-Danilo.local>
2015-08-09 21:20:32 -0300

    init mobile

--- src/build/main.js
+++ src/build/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
+
+	/**
+	*
+	* avise-me
+	*
+	**/
+	
 	var $avisme = $('.avise-me'),
 		$pageHomeAction = $('.page-home_action');
 
@@ -7,6 +14,14 @@ $(document).ready(function(){
 		$pageHomeAction.addClass('form-active');
 
 		return false;
+	});
+
+
+
+	// lazy load
+	$('[data-src]').each(function(){
+		var img = $(this).data('src');
+		$(this).attr('src', img);	
 	});
 
 });
