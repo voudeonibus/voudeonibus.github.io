18d259b1dbb158c54e2df5b469ab87f8886c10b5
Danilo Woznica <danilowoznica@MacBook-Pro-de-Danilo.local>
2015-08-09 23:06:49 -0300

    novo item no menu

--- src/build/main.js
+++ src/build/main.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
 
-
-	/**
-	*
-	* avise-me
-	*
-	**/
-	
 	var $avisme = $('.avise-me'),
 		$pageHomeAction = $('.page-home_action');
 
@@ -14,14 +7,6 @@ $(document).ready(function(){
 		$pageHomeAction.addClass('form-active');
 
 		return false;
-	});
-
-
-
-	// lazy load
-	$('[data-src]').each(function(){
-		var img = $(this).data('src');
-		$(this).attr('src', img);	
 	});
 
 });
