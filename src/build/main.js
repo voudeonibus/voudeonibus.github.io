cf2a38353ef10979f600a65d2143d4d57736d0da
Danilo Woznica <danilowoznica@MacBook-Pro-de-Danilo.local>
2015-08-12 23:28:44 -0300

    cadastro email

--- src/build/main.js
+++ src/build/main.js
@@ -1,12 +1,39 @@
 $(document).ready(function(){
 
 	var $avisme = $('.avise-me'),
-		$pageHomeAction = $('.page-home_action');
+		$pageHomeAction = $('.page-home_action'),
+		$form = $avisme.find('form');
 
 	$avisme.find('button').click(function(){
-		$pageHomeAction.addClass('form-active');
 
-		return false;
+		if (!$pageHomeAction.hasClass('form-active')) {
+			$pageHomeAction.addClass('form-active');
+			return false;
+		}
 	});
+
+
+
+
+	function register($form) {
+		console.log('função');
+	    $.ajax({
+	        type: $form.attr('method'),
+	        url: $form.attr('action'),
+	        data: $form.serialize(),
+	        cache       : false,
+	        dataType    : 'json',
+	        contentType: "application/json; charset=utf-8",
+	        error       : function(err) { alert("Could not connect to the registration server. Please try again later."); },
+	        success     : function(data) {
+	            if (data.result != "success") {
+	            	console.log('sucesso');
+	                // Something went wrong, do something to notify the user. maybe alert(data.msg);
+	            } else {
+	                // It worked, carry on...
+	            }
+	        }
+	    });
+	}
 
 });
