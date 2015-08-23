7e334b4846f071892296c270e6e3b8cb175dafe9
danilowzn <danilowoznica@MacBook-Pro-de-Danilo.local>
2015-08-23 12:23:27 -0300

    Ajustes gerais

---
+++ src/build/social.js
@@ -0,0 +1,1 @@
+!function(e,t,n){var o,c=e.getElementsByTagName(t)[0];e.getElementById(n)||(o=e.createElement(t),o.id=n,o.src="//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.4",c.parentNode.insertBefore(o,c))}(document,"script","facebook-jssdk");
