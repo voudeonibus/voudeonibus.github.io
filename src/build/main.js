e05ec488a2faf82b3dd91585c201600c4694c646
Danilo W <danilo_wzn@hotmail.com>
2016-02-09 21:05:28 -0200

    Adaptações para o primeiro lançamento :rocket: :full_moon:

--- src/build/main.js
+++ src/build/main.js
@@ -1,1 +1,145 @@
-$(document).ready(function(){var a=$(".avise-me"),i=$(".page-home_action"),s=(a.find("form"),$(".mobile-sections_vouagora")),e=$(".double-mac_with-bacon"),o=$(".nav-site nav"),t=$(".logo-site, .download"),n=$(".page-oapp");isMobile&&(e.click(function(){$(this).toggleClass("active"),o.slideToggle()}),o.on("click","a",function(){var a=$(this).data("target");$("html,body").animate({scrollTop:$(a).offset().top-56},1e3),o.slideUp(),e.removeClass("active")}),t.click(function(){$("html,body").animate({scrollTop:0},1e3),o.slideUp(),e.removeClass("active")}),$(window).scroll(function(){var a=$(window).scrollTop(),i=n.offset().top;a>=i?$(".nav-site").addClass("fixed"):$(".nav-site").removeClass("fixed")})),a.find("button").click(function(){return i.hasClass("form-active_ios")||i.hasClass("form-active_android")?void 0:($(this).parents(".avise-me").hasClass("android")?(i.addClass("form-active_android"),ga("send","event","download","android-avise-me","avise-me")):$(this).parents(".avise-me").hasClass("ios")&&(i.addClass("form-active_ios"),ga("send","event","download","ios-avise-me","avise-me")),!1)}),a.find(".close").click(function(){i.removeClass("form-active_ios").removeClass("form-active_android"),ga("send","event","download","ios-avise-me","close")}),isMobile&&$("[data-src]").each(function(){var a=$(this).data("src");$(this).attr("src",a)}),isMobile&&s.owlCarousel({stagePadding:50,items:1,afterAction:function(){var a=this.currentItem+1;$(".dots a:nth-child("+a+")").addClass("active").siblings().removeClass("active")}}),$(".dots").on("click","a",function(){var a=$(this).index();s.trigger("owl.goTo",a),$(this).addClass("active").siblings().removeClass("active")})}),$(window).load(function(){var a=window.location.hash;"#equipe"==a&&$(".nav-site a:nth-child(5)").click()});
+
+
+$(document).ready(function(){
+
+
+	// para variar
+	var $avisme = $('.avise-me'),
+		$pageHomeAction = $('.page-home_action'),
+		$form = $avisme.find('form'),
+		$slideVouagora = $('.mobile-sections_vouagora'),
+		$menuBurguer = $('.double-mac_with-bacon'),
+		$navSite = $('.nav-site nav'),
+		$logoSite = $('.logo-site, .download'),
+		$pageOapp = $('.page-oapp');
+
+
+	if (isMobile) {
+		// menu hamburguer click
+		$menuBurguer.click(function(){
+			$(this).toggleClass('active');
+			$navSite.slideToggle();
+
+		});
+
+		// scroll to section
+		$navSite.on('click', 'a', function(){
+			var target = $(this).data('target');
+
+			$('html,body').animate({
+	          scrollTop: $(target).offset().top - 56
+	        }, 1000);
+
+	        $navSite.slideUp();
+	        $menuBurguer.removeClass('active');
+		});
+
+		// scroll to top
+		$logoSite.click(function(){
+			$('html,body').animate({
+	          scrollTop: 0
+	        }, 1000);
+
+	        $navSite.slideUp();
+	        $menuBurguer.removeClass('active');
+		});
+
+		// show menu
+		$(window).scroll(function(){
+			var ws = $(window).scrollTop(),
+				of = $pageOapp.offset().top;
+
+			if(ws >= of) {
+				$('.nav-site').addClass('fixed');
+			} else {
+				$('.nav-site').removeClass('fixed');
+			}
+		});
+
+
+	};
+
+	/**
+	*
+	* avise me
+	*
+	**/
+
+	$avisme.find('button').click(function(){
+
+		if (!$pageHomeAction.hasClass('form-active_ios') && !$pageHomeAction.hasClass('form-active_android')) {
+
+			if($(this).parents('.avise-me').hasClass('android')) {
+				$pageHomeAction.addClass('form-active_android');
+				ga('send', 'event', 'download', 'android-avise-me', 'avise-me');
+			} else if ($(this).parents('.avise-me').hasClass('ios')) {
+				$pageHomeAction.addClass('form-active_ios');
+				ga('send', 'event', 'download', 'ios-avise-me', 'avise-me');
+			}
+
+			return false;
+
+		}
+
+	});
+
+	$avisme.find('.close').click(function(){
+		$pageHomeAction.removeClass('form-active_ios').removeClass('form-active_android');
+		ga('send', 'event', 'download', 'ios-avise-me', 'close');
+	});
+
+
+	/**
+	*
+	* load images mobile
+	*
+	**/
+	if(isMobile) {
+		// lazy load
+		$('[data-src]').each(function(){
+			var img = $(this).data('src');
+			$(this).attr('src', img);
+		});
+	}
+
+
+
+	/**
+	*
+	* page vou agora
+	*
+	**/
+	if (isMobile) {
+		$slideVouagora.owlCarousel({
+			stagePadding: 50,
+			items: 1,
+			afterAction : function() {
+				var current = this.currentItem+1;
+				$('.dots a:nth-child('+current+')').addClass('active').siblings().removeClass('active');
+			}
+		});
+	};
+
+	$('.dots').on('click', 'a', function(){
+		var target = $(this).index();
+		$slideVouagora.trigger('owl.goTo', target);
+		$(this).addClass('active').siblings().removeClass('active');
+	});
+
+
+
+});
+
+/**
+*
+* link equipe
+*
+*/
+$(window).load(function(){
+	var hash = window.location.hash;
+	if(hash == "#equipe") {
+		$('.nav-site a:nth-child(5)').click();
+	}
+});
+
+
